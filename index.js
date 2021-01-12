b6a2c700759d3fe76b58416d49b5bb066dd33a3a
Drothschild89 <Drothschild89@gmail.com>
2021-01-12 10:39:21 -0800

    Done.

--- index.js
+++ index.js
@@ -11,3 +11,24 @@ function handleClick(e) {
 
 /* Write your implementation of greet() */
 /* Write your implementation of displayMessage() */
+
+function greet(str){
+  let toRemoveColon = str.split(':').join('');
+  let toNumber = parseInt(toRemoveColon);
+
+  if(toNumber < 1200){
+    return "Good Morning"
+  } else if(toNumber > 1200 && toNumber < 1700){
+    return "Good Afternoon"
+  } else {
+    return "Good Evening"
+  }
+}
+
+function displayMessage(str){
+  document.getElementById('greeting').innerText = str
+}
+
+// let main = document.querySelector('main');
+// let newHeader = document.createElement('h1')
+// newHeader.innerHTML = 'Dustin is the champion';
